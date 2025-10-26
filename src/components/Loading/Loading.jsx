cdcc6b477d918e5f60030073f8d8b750af22d3d0
whoami 404 <Cicada.3301.M.A.110@gmail.com>
2025-10-26 19:55:18 +0330

    fix some bugs

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import LoadingImage from "../../assets/logo/logo.webp";
 const Loading = () => {
     return (
         <div className="fixed inset-0 bg-black/70 backdrop-blur-xl flex justify-center items-center z-101">
-            <img src={LoadingImage} alt="loading" className="w-1/4" />
+            <img src={LoadingImage} alt="loading" className="w-2/3 md:w-1/4" />
         </div>
     );
 }
