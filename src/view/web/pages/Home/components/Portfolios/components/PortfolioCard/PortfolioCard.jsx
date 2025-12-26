732d0d44e56054177bf1db8f1cfe38fee8ff4ab7
whoami 404 <Cicada.3301.M.A.110@gmail.com>
2025-12-26 18:57:20 +0330

    fix portfolio's card responsive bug

--- src/view/web/pages/Home/components/Portfolios/components/PortfolioCard/PortfolioCard.jsx
+++ src/view/web/pages/Home/components/Portfolios/components/PortfolioCard/PortfolioCard.jsx
@@ -1,9 +1,11 @@
+import "./PortfolioCard.css";
+
 const PortfolioCard = ({ data }) => {
     const { name, shortDescription, technologies, image, link, github, date } = data || {};
 
     return (
-        <div className="min-w-67 max-w-67 xs:!min-w-85 xs:!max-w-85 h-130 max-xs:!h-120 px-4 py-6 rounded-2xl flex flex-col justify-start items-start bg-gradient-to-bl from-[#151b29] to-[#0b0f19] transition hover:-translate-y-2 hover:border-teal-700 duration-300 text-white border border-slate-800 relative">
-                        <h4 className="text-2xl font-ubuntuMedium capitalize">
+        <div className="min-w-67 max-w-67 xs:!min-w-85 xs:!max-w-85 h-130 max-xs:h-135 px-4 py-6 rounded-2xl flex flex-col justify-start items-start bg-gradient-to-bl from-[#151b29] to-[#0b0f19] transition hover:-translate-y-2 hover:border-teal-700 duration-300 text-white border border-slate-800 relative">
+                        <h4 className="text-2xl max-xs:text-xl font-ubuntuMedium capitalize">
                             {name}
                         </h4>
                         <p className="text-sm text-slate-400 capitalize mt-1">
@@ -13,11 +15,11 @@ const PortfolioCard = ({ data }) => {
                             <img src={image} alt={name || "portfolio"} className="rounded-lg size-full object-cover transition-all duration-300 hover:scale-110" />
                         </div>
                         <div className="w-full flex justify-between items-center mb-4">
-                            <a href={link} className="z-90 border border-teal-500 text-teal-500 py-1.5 px-4 rounded-md transition-all duration-300 hover:-translate-y-1 capitalize">
+                            <a href={link} className="card-btn max-xs:text-sm">
                                 view project
                             </a>
                             {github &&
-                                <a href={github} className="z-90 border border-teal-500 text-teal-500 py-1.5 px-4 rounded-md transition-all duration-300 hover:-translate-y-1 capitalize">
+                                <a href={github} className="card-btn max-xs:text-sm">
                                     github repo
                                 </a>
                             }
